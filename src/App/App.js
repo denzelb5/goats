f62b8065c9b356dab407d4180c188e737f010855
Denise Baker <denviol@yahoo.com>
2019-12-09 19:24:06 -0600

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import GoatCorral from '../components/GoatCorral/GoatCorral';
+// import logo from './logo.svg';
+import goatData from '../helpers/data/goatData';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-danger">Bootstrap Button</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    this.setState({ goats });
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <button className="btn btn-danger">Bootstrap Button</button>
+      <GoatCorral butts={this.state.goats} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
